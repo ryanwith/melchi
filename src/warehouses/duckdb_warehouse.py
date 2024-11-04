2dc8ac4215c0a9bb843a3cc699dee02894149f50
Ryan Waldorf <ryan.m.waldorf@gmail.com>
2024-11-04 16:21:29 -0500

    same

--- src/warehouses/duckdb_warehouse.py
+++ src/warehouses/duckdb_warehouse.py
@@ -398,7 +398,9 @@ class DuckDBWarehouse(AbstractWarehouse):
 
     def convert_list_to_duckdb_syntax(self, python_list):
         """Converts Python list to DuckDB array syntax."""
-        return f"[{', '.join(list(map(lambda item: f"'{item}'", python_list)))}]"
+        quoted_items = [f"'{item}'" for item in python_list]  # or map(lambda x: f"'{x}'", python_list)
+        joined_items = ', '.join(quoted_items)
+        return f"[{joined_items}]"    
     
     def format_value_for_insert(self, value):
         if type(value) == str:
